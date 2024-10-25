664da009fc9f61abc6bd7b09dd7d7b7233892f80
Sufiyan Khan <sufiyan.khan@multidots.com>
2024-10-25 22:47:51 +0530

    Refine and redesign API Block

--- includes/blocks/sab/build/index.asset.php
+++ includes/blocks/sab/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '27eb03025c03e5312288');
+<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '80134a81955b4c6c8b54');
